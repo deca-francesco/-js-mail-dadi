f58f20afb17942aca86f2547f06a20d46b4ed8f3
Francesco De Carlo <francescodecarlo70@gmail.com>
2024-10-03 16:35:16 +0200

    ADD: email check and alert for user

--- main.js
+++ main.js
@@ -7,12 +7,21 @@ Chiedi all’utente la sua email, controlla che sia nella lista di chi può acce
 
 // Variabili
 let email;
-let user_list = [];
+let user_list = ["email1", "email2", "email3"];
+console.log(user_list);
 
 // Dati
 // creo un prompt che mi restituisce l'email dell'utente
 email = prompt("Inserisci la tua email");
 console.log(email);
 
-// Elaborazione
-// creo una condizionale che verifica che la mail sia presente nell'array
+// Elaborazione e output
+// creo una condizionale che verifica che la mail sia presente nell'array e restituisco un alert all'user
+if (user_list.includes(email)) {
+    console.log(`la mail ${email} è presente nell'array`);
+    alert("Utente riconosciuto");
+}
+else {
+    console.log(`la mail ${email} NON è presente nell'array`);
+    alert("Utente NON riconosciuto!");
+}
