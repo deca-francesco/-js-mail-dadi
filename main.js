43f21ed13ffe852b9e9ff8f4f03eb1da91dd9246
Francesco De Carlo <francescodecarlo70@gmail.com>
2024-10-03 16:20:42 +0200

    ADD: variables and prompt to collect email

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 console.log("js linked");
+
+/*
+Esercizio Mail
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+*/
+
+// Variabili
+let email;
+let user_list = [];
+
+// Dati
+// creo un prompt che mi restituisce l'email dell'utente
+email = prompt("Inserisci la tua email");
+console.log(email);
+
+// Elaborazione
+// creo una condizionale che verifica che la mail sia presente nell'array
