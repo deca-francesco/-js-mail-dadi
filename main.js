10a9782f90571df10cd4adb8ec5eb1ef668f68d6
deca-francesco <francescodecarlo70@gmail.com>
2024-10-04 10:37:17 +0200

    ADD: lesson extras

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ Chiedi all’utente la sua email, controlla che sia nella lista di chi può acce
 // Variabili
 let email;
 let user_list = ["email1", "email2", "email3"];
+// let can_access = false;
 console.log(user_list);
 
 // Dati
@@ -17,6 +18,21 @@ console.log(email);
 
 // Elaborazione e output
 // creo una condizionale che verifica che la mail sia presente nell'array e restituisco un alert all'user
+
+
+// includes cicla nell'array, quindi è lo stesso che scrivere un ciclo for
+/*
+for (let i = 0; i < user_list.length; i++) {
+    // prendo il singolo elemento dell'array
+    const user_index = user_list[i];
+
+    //controllo se c'è nell'array
+    if (user_index == email) {
+        can_access = true;
+        break;      // interrompiamo il ciclo appena trova l'email
+    }
+}
+*/
 if (user_list.includes(email)) {
     console.log(`la mail ${email} è presente nell'array`);
     alert("Utente riconosciuto");
@@ -35,10 +51,11 @@ Prima di partire a scrivere codice poniamoci qualche domanda:
 Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
 */
 
+/*
 // Variabili
 let user_score;
 let cpu_score;
-let winner_is = "Il vincitore è: ";
+const winner_is = "Il vincitore è: ";
 let winner;
 
 // Elaborazione
@@ -58,8 +75,51 @@ else if (user_score = cpu_score) {
 }
 
 // output
-console.log(`GIOCATORE: ${user_score} CPU ${cpu_score}`);
+console.log(`GIOCATORE: ${user_score} | CPU ${cpu_score}`);
 console.log(winner_is, winner);
 alert(`GIOCATORE: ${user_score} 
 CPU: ${cpu_score} 
 ${winner_is} ${winner}`);
+*/
+
+// continuiamo a far andare il gioco
+
+let play_again = true;
+
+// usiamo il ciclo while sfruttando la facilità con cui facciamo cicli infiniti
+// è come un ciclo for senza incremento
+while (play_again) {
+    // Variabili
+    let user_score;
+    let cpu_score;
+    const winner_is = "Il vincitore è: ";
+    let winner;
+
+    // Elaborazione
+    // genero i 2 numeri random e li assegno alle variabili
+    user_score = Math.floor(Math.random() * 6) + 1;
+    cpu_score = Math.floor(Math.random() * 6) + 1;
+
+    // confronto i punteggi per vedere chi vince
+    if (user_score > cpu_score) {
+        winner = "GIOCATORE";
+    }
+    else if (user_score < cpu_score) {
+        winner = "CPU";
+    }
+    else if (user_score = cpu_score) {
+        winner = "NESSUNO, PARITà!";
+    }
+
+    // output
+    console.log(`GIOCATORE: ${user_score} | CPU ${cpu_score}`);
+    console.log(winner_is, winner);
+    alert(`GIOCATORE: ${user_score} 
+CPU: ${cpu_score} 
+${winner_is} ${winner}`);
+
+    play_again = confirm("Giochi ancora?")      // confirm
+}
+
+console.log("Ok, alla prossima!");
+alert("Alla prossima!")
